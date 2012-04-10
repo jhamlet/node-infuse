55339d2832ac2acd4cc4ac98c462aa59fb0e3343
Jerry Hamlet <jerry@hamletink.com>
2012-04-10 10:32:59 -0700

    Add ast-util module for working with AST to values, and vice versa. Add tests for ast-util, and samples for working with defines.

--- test/sample-module.js
+++ test/sample-module.js
@@ -8,17 +8,31 @@ var values = {
         ],
         "number": 9,
         "boolean": true,
-        "string": "string"
+        "string": "string",
+        "undefined": undefined
     },
-    ASTValues = {
-        "true": ["name", "true"],
-        "false": ["name", "false"],
-        "undefined": ["name", "undefined"]
+    STRINGS = {
+        "UI.STRING.TITLE": "The title",
+        "UI.STRING.SUB-TITLE": "The sub-title"
     },
     env = process.env.ENVIRONMENT || "dev"
 ;
 
 module.exports = {
-    ENVIRONMENT: ["string", env],
-    DEBUG: env === "dev" ? ASTValues["true"] : ASTValues["false"]
+    TITLE_KEY: "UI.STRING.TITLE",
+    ENVIRONMENT: env,
+    DEBUG: env === "dev" ? true : false,
+    STRINGS: function (key) {
+        return STRINGS[key] || "";
+    },
+    MAKE_SPECIAL_METHOD: function (env) {
+        return env === "dev" ? function () {
+            return "DEV";
+        } : function () {
+            return "NOT DEV";
+        };
+    },
+    EXEC: function (fn) {
+        return fn();
+    }
 };
--- test/sample-src.js
+++ test/sample-src.js
@@ -1,12 +1,29 @@
 
 var _ = require("underscore"),
     Iterator = require("../lib/iterator.js"),
-    iter = new Iterator()
+    iter     = new Iterator(),
+    title    = STRINGS(TITLE_KEY),
+    subtitle = STRINGS("UI.STRING.SUB-TITLE"),
+    nothing  = STRINGS(),
+    MyClass
 ;
 
-if (ENVIRONMENT === "dev" || DEBUG) {
+if (ENVIRONMENT === "dev") {
     console.log("Development environment");
 }
 
 console.log(_.map([1, 2, 3], function (i) { return Math.pow(i, 2); }));
 
+MyClass = function () {
+    
+}
+
+MyClass.prototype = {
+    env: MAKE_SPECIAL_METHOD(ENVIRONMENT),
+            buildOs: EXEC(function () {
+                return require("os").hostname;
+            })
+};
+
+var obj = new MyClass();
+console.log(obj.env());
