baabd1b9ed031bf4cb423dfb4745ab29168fd1c2
Jerry Hamlet <jerry@hamletink.com>
2012-11-07 20:04:47 -0800

    Modify infuser to work on a filepath instead of an AST.

--- test/test-infuser.js
+++ test/test-infuser.js
@@ -1,58 +1,59 @@
 /*globals suite, setup, test */
 
-var Infuser = require("infuse/infuser"),
-    Ast = require("infuse/ast"),
-    source = "var foo = \"foo\", baz = \"baz\"; foo = foo + baz;"
+var Infuser = require('infuse/infuser'),
+    Ast = require('infuse/ast'),
+    FS  = require('fs'),
+    source = 'var foo = \'foo\', baz = \'baz\', fn = require("fn"), bz = window.require("bz"); foo = foo + baz;',
+    srcfile = 'test-infuse-source.js'
 ;
 
-suite("Infuser", function () {
+suite('Infuser', function () {
     
-    test("All Identifier nodes", function () {
-        var ast = new Ast({
-                source: source
-            }),
-            infuser = new Infuser(),
+    setup(function () {
+        FS.writeFileSync(srcfile, source, "utf8");
+    });
+    
+    teardown(function () {
+        FS.unlink(srcfile);
+    });
+    
+    test('All Identifier nodes', function () {
+        var infuser = new Infuser(),
             seen = 0
         ;
         
         infuser.use({
             'Identifier': function (node, infuser) {
                 seen++;
-                node.type.should.equal("Identifier");
+                node.type.should.equal('Identifier');
             }
         });
         
-        infuser.run(ast);
+        infuser.run(srcfile);
         
-        seen.should.equal(5);
+        seen.should.equal(10);
     });
     
-    test("Identifier[name='foo']", function () {
-        var ast = new Ast({
-                source: source
-            }),
-            infuser = new Infuser(),
+    test('Identifier[name="foo"]', function () {
+        var infuser = new Infuser(),
             seen = 0
         ;
         
         infuser.use({
             'Identifier[name="foo"]': function (node, infuser) {
                 seen++;
-                node.type.should.equal("Identifier");
-                node.name.should.equal("foo");
+                node.type.should.equal('Identifier');
+                node.name.should.equal('foo');
             }
         });
         
-        infuser.run(ast);
+        infuser.run(srcfile);
         
         seen.should.equal(3);
     });
     
-    test("CallExpression > Identifier[name = require]", function () {
-        var ast = new Ast({
-                source: "var foo = require('foo'), baz = window.require('baz');"
-            }),
-            infuser = new Infuser(),
+    test('CallExpression > Identifier[name = require]', function () {
+        var infuser = new Infuser(),
             bareSeen = 0,
             totalSeen = 0
         ;
@@ -61,17 +62,17 @@ suite("Infuser", function () {
         
         infuser.use({
             // this should fire both 'require' statements
-            "CallExpression Identifier[name='require']": function (node, infuser) {
+            'CallExpression Identifier[name="require"]': function (node, infuser) {
                 totalSeen++;
             },
             // this should only fire for the bare 'require'
-            "call.callee > id[name = require]": function (node, infuser) {
+            'call.callee > id[name = require]': function (node, infuser) {
                 bareSeen++;
-                node.parent.arguments[0].value.should.equal("foo");
+                node.parent.arguments[0].value.should.equal('fn');
             }
         });
         
-        infuser.run(ast);
+        infuser.run(srcfile);
         
         bareSeen.should.equal(1);
         totalSeen.should.equal(2);
