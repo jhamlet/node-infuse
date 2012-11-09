42865e11caa9c4bb277415b0b60aa032dfdeeb1f
Jerry Hamlet <jerry@hamletink.com>
2012-11-08 20:11:54 -0800

    Get defines infusion working. Fixed some errors in infuser. Associated tests for defines too.

---
+++ test/src-infusion-defines.js
@@ -0,0 +1,6 @@
+
+var foo = INFUSE_FOO,
+    baz = INFUSE_BAZ(),
+    bar = INFUSE_BAR(INFUSE_FOO)
+;
+
---
+++ test/test-infusion-defines.js
@@ -0,0 +1,113 @@
+/*globals suite, setup, test */
+
+var Infuser = require('infuse/infuser'),
+    definesInfusion = require('infuse/infusions/defines.js'),
+    srcfile = __dirname + '/src-infusion-defines.js'
+;
+
+suite('Infusions - defines', function () {
+    
+    test('Simple replacement', function () {
+        var infuser = new Infuser(),
+            ast
+        ;
+        
+        infuser.use(definesInfusion({
+            INFUSE_FOO: 'foo'
+        }));
+        
+        ast = infuser.run(srcfile);
+        ast.subject.body[0].declarations[0].id.name.should.equal('foo');
+        ast.subject.body[0].declarations[0].init.value.should.equal('foo');
+    });
+    
+    test('Array replacement', function () {
+        var infuser = new Infuser(),
+            obj = {
+                INFUSE_FOO: [1, 2, 3, 4]
+            },
+            ast
+        ;
+        
+        infuser.use(definesInfusion(obj));
+        
+        ast = infuser.run(srcfile);
+        ast.subject.body[0].declarations[0].id.name.should.equal('foo');
+        ast.subject.body[0].declarations[0].init.type.should.equal('ArrayExpression');
+        
+        ast.subject.body[0].declarations[0].init.elements.forEach(function (node, idx) {
+            node.type.should.equal('Literal');
+            node.value.should.equal(obj.INFUSE_FOO[idx]);
+        });
+    });
+    
+    test('Function replacement', function () {
+        var infuser = new Infuser(),
+            obj = {
+                INFUSE_FOO: function () {
+                    return 'foo';
+                }
+            },
+            ast
+        ;
+        
+        infuser.use(definesInfusion(obj));
+        
+        ast = infuser.run(srcfile);
+        ast.subject.body[0].declarations[0].id.name.should.equal('foo');
+        ast.subject.body[0].declarations[0].init.type.should.equal('FunctionExpression');
+    });
+
+    test('Call function replacement', function () {
+        var infuser = new Infuser(),
+            obj = {
+                INFUSE_BAZ: function () {
+                    return 'baz';
+                }
+            },
+            ast
+        ;
+        
+        infuser.use(definesInfusion(obj));
+        
+        ast = infuser.run(srcfile);
+        ast.subject.body[0].declarations[1].id.name.should.equal('baz');
+        ast.subject.body[0].declarations[1].init.type.should.equal('Literal');
+        ast.subject.body[0].declarations[1].init.value.should.equal('baz');
+    });
+    
+    test('Call function replacement with nested defines', function () {
+        var infuser = new Infuser(),
+            obj = {
+                INFUSE_FOO: 'foo',
+                INFUSE_BAR: function (name) {
+                    return name + '-bar';
+                }
+            },
+            ast
+        ;
+        
+        infuser.use(definesInfusion(obj));
+        
+        ast = infuser.run(srcfile);
+        ast.subject.body[0].declarations[2].id.name.should.equal('bar');
+        ast.subject.body[0].declarations[2].init.type.should.equal('Literal');
+        ast.subject.body[0].declarations[2].init.value.should.equal('foo-bar');
+    });
+    
+    test('Call function replacement that isn\'t defined as a function throws an Error', function () {
+        var infuser = new Infuser(),
+            obj = {
+                INFUSE_FOO: 'foo',
+                INFUSE_BAR: 'bar'
+            },
+            ast
+        ;
+        
+        infuser.use(definesInfusion(obj));
+        
+        (function () {
+            ast = infuser.run(srcfile);
+        }).should.throw();
+    });
+});
