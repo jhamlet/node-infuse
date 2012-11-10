423a2d01be81227888d152af4cdf8470412ad60f
Jerry Hamlet <jerry@hamletink.com>
2012-11-09 16:22:57 -0800

    Get ready for testing the requires infusion.

---
+++ test/test-infusion-requires.js
@@ -0,0 +1,16 @@
+/*globals suite, setup, test */
+
+var Infuser  = require('infuse/infuser'),
+    requires = require('infuse/infusions/requires'),
+    srcfile  = 'test-src/src-infusion-defines.js'
+;
+
+suite('Infuser - Requires', function () {
+    
+    suite('Direct interaction with infuser', function () {
+        test('One', function () {
+            
+        });
+    });
+    
+});
