627862328017817417a9162b1fe39dfaa82fb1ab
Jerry Hamlet <jerry@hamletink.com>
2012-11-12 14:01:09 -0800

    Playing around with comments in code.

--- test-src/extracts-insertions.js
+++ test-src/extracts-insertions.js
@@ -1,6 +1,18 @@
-
-var foo = INFUSE_FOO,
-    baz = INFUSE_BAZ(),
-    bar = INFUSE_BAR(INFUSE_FOO),
-    barBaz = INFUSE_BAR(INFUSE_BAZ(INFUSE_FOO));
-
+/**
+ * Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ * tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim
+ * veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+ * commodo consequat.  Duis aute irure dolor in reprehenderit in voluptate
+ * velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat
+ * cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+ * id est laborum.
+ */
+// comment 1
+var foo = INFUSE_FOO, // comment 1.1
+    // comment 2
+    baz = INFUSE_BAZ(), // comment 2.1
+    // comment 3
+    bar = INFUSE_BAR(INFUSE_FOO), // comment 3.1
+    // comment 4
+    barBaz = INFUSE_BAR(INFUSE_BAZ(INFUSE_FOO)); // comment 4.1
+    // comment 5
