c23415442f2803aa12c1be66348be6c8590f36f5
Jerry Hamlet <jerry@hamletink.com>
2012-11-11 18:47:26 -0800

    Add #query method and update the test file.

--- test/test-ast.js
+++ test/test-ast.js
@@ -1,61 +1,69 @@
 /*globals suite, test */
 
-var Ast = require("infuse/ast"),
-    FS  = require("fs"),
-    should = require("should"),
-    source  = "var foo = \"foo\";",
-    filepath = "test/test-ast-sample.js",
+var Ast = require('infuse/ast'),
+    FS  = require('fs'),
+    should = require('should'),
+    source  = 'var foo = "foo";',
+    filepath = 'test-src/ast.js',
     ast
 ;
 
-suite("InfuseAst", function () {
+suite('AST', function () {
     
     setup(function () {
         ast = new Ast(source);
-        FS.writeFileSync(filepath, "var baz = \"baz\";", "utf8");
     });
     
-    teardown(function () {
-        FS.unlinkSync(filepath);
-    });
-    
-    test("Static properties exist", function () {
+    test('Static properties exist', function () {
         Ast.TRAVERSAL_OPTIONS.BREAK.should.equal(1);
         Ast.TRAVERSAL_OPTIONS.SKIP.should.equal(2);
         Ast.TRAVERSAL_OPTIONS.REVISIT.should.equal(3);
     });
     
-    test("Subject parsed correctly", function () {
-        ast.subject.should.be.a("object");
-        ast.subject.body[0].type.should.equal("VariableDeclaration");
+    test('Subject parsed correctly', function () {
+        ast.subject.should.be.a('object');
+        ast.subject.body[0].type.should.equal('VariableDeclaration');
         ast.parsed.should.equal(true);
         ast.generated.should.equal(false);
     });
     
-    test("Source correctly computed", function () {
-        ast.subject.should.be.a("object");
+    test('Source correctly computed', function () {
+        ast.subject.should.be.a('object');
         ast.parsed.should.equal(true);
         ast.generated.should.equal(false);
         ast.source.should.equal(source);
         ast.generated.should.equal(true);
     });
     
-    test("Source from file", function () {
+    test('Source from file', function () {
         var ast = new Ast({ file: filepath });
-        ast.subject.should.be.a("object");
-        ast.subject.body[0].type.should.equal("VariableDeclaration");
+        ast.subject.should.be.a('object');
+        ast.subject.body[0].type.should.equal('VariableDeclaration');
         ast.parsed.should.equal(true);
         ast.generated.should.equal(false);
-        ast.source.should.equal(FS.readFileSync(filepath, "utf8"));
+        ast.source.should.equal(FS.readFileSync(filepath, 'utf8'));
         ast.generated.should.equal(true);
     });
 
-    test("Setting file property resets source", function () {
+    test('Setting file property resets source', function () {
         ast.source.should.equal(source);
         ast.file = filepath;
         should.not.exist(ast.options.source);
         ast.source.should.not.equal(source);
-        ast.source.should.equal(FS.readFileSync(filepath, "utf8"));
+        ast.source.should.equal(FS.readFileSync(filepath, 'utf8'));
         ast.generated.should.equal(true);
+    });
+    
+    test('Simple Query', function () {
+        var nodes = ast.query('Identifier[name="foo"]');
+        
+        nodes.length.should.equal(1);
+        nodes[0].type.should.equal('Identifier');
+        nodes[0].name.should.equal('foo');
+        
+        nodes = ast.query('lit[value="foo"]');
+        nodes.length.should.equal(1);
+        nodes[0].type.should.equal('Literal');
+        nodes[0].value.should.equal('foo');
     });
 });
