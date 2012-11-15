cf77888ffb20f05964612427c687d283bca6ec82
Jerry Hamlet <jerry@hamletink.com>
2012-11-14 19:05:48 -0800

    Requires handled...

--- test/test-extracts-requires.js
+++ test/test-extracts-requires.js
@@ -1,15 +1,14 @@
 /*globals suite, setup, test */
 
 var Infuser  = require('infuse/infuser'),
-    requires = require('infuse/extracts/requires')
+    Requires = require('infuse/extracts/requires')
 ;
 
 suite('Infuser - Requires', function () {
     
-    suite('Direct interaction with infuser', function () {
-        test('One', function () {
-            
-        });
+    test('One Require', function () {
+        var infuser = new Infuser();
+        infuser.use(new Requires());
+        infuser.run('test-src/extracts-requires.js');
     });
-    
 });
