93baa0e77df37cd8bddf7a5a75423ad085beafc9
Jerry Hamlet <jerry@hamletink.com>
2013-04-16 07:44:22 -0700

    Pass in the resolve function to the requires infusion.

--- test/test-infusions-requires.js
+++ test/test-infusions-requires.js
@@ -5,11 +5,31 @@ var Infuser  = require('infuse/infuser'),
     Ast      = require('infuse/ast')
 ;
 
-suite('Infuser - Requires', function () {
+suite('Infusions - Requires', function () {
     
-    test('One Require', function () {
+    test('resolve', function () {
         var infuser = new Infuser();
-        infuser.use(new Requires());
+        infuser.use(new Requires({
+            resolve: require('infuse/resolve')
+        }));
         infuser.run(new Ast({ file: 'test-src/infusions-requires.js'}));
+    });
+    
+    test('resolveIt', function () {
+        var infuser = new Infuser();
+        infuser.use(new Requires({
+            resolve: function (name, startDir) {
+                return require('resolveit').sync(name, startDir);
+            }
+        }));
+        infuser.run(new Ast({ file: 'test-src/infusions-requires.js'}));
+    });
+    
+    test('no resolve function throws an error', function () {
+        var infusion;
+        
+        (function () {
+            infusion = new Requires();
+        }).should.throw()
     });
 });
